67a4f51bb5d85dc39d7bb8ceacf89b5a4db88ebd
Alvin De Cruz <decruz@gmail.com>
2018-05-21 11:10:22 +0800

    first commits

---
+++ src/config.js
@@ -0,0 +1,17 @@
+export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
+    s3: {
+      REGION: "ap-southeast-1",
+      BUCKET: "notes-app-uploads-amg"
+    },
+    apiGateway: {
+      REGION: "ap-southeast-1",
+      URL: "https://d4ygenf2ml.execute-api.ap-southeast-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "ap-southeast-1",
+      USER_POOL_ID: "ap-southeast-1_lRPPfJTy5",
+      APP_CLIENT_ID: "cd321b0bpm674jvj0agtc4jif",
+      IDENTITY_POOL_ID: "ap-southeast-1:bf7afc5e-f5e6-4bcd-8676-770d755aef7b"
+    }
+  };
