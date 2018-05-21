f5efe35457ea8be5a1f50b0628eb4c9720412f15
Alvin De Cruz <decruz@gmail.com>
2018-05-21 11:27:05 +0800

    configuring environments

--- src/config.js
+++ src/config.js
@@ -1,24 +1,24 @@
 const dev =  {
     s3: {
       REGION: "ap-southeast-1",
-      BUCKET: "notes-app-uploads-amg"
+      BUCKET: "notes-app-2-api-dev-attachmentsbucket-4gjozwdi9v2v"
     },
     apiGateway: {
       REGION: "ap-southeast-1",
-      URL: "https://d4ygenf2ml.execute-api.ap-southeast-1.amazonaws.com/prod"
+      URL: "https://tuj16nq3wg.execute-api.ap-southeast-1.amazonaws.com/dev"
     },
     cognito: {
       REGION: "ap-southeast-1",
-      USER_POOL_ID: "ap-southeast-1_lRPPfJTy5",
-      APP_CLIENT_ID: "cd321b0bpm674jvj0agtc4jif",
-      IDENTITY_POOL_ID: "ap-southeast-1:bf7afc5e-f5e6-4bcd-8676-770d755aef7b"
+      USER_POOL_ID: "ap-southeast-1_4gpubMqq5",
+      APP_CLIENT_ID: "3aj70ir6sm7jbvkvqh5f0lkaqn",
+      IDENTITY_POOL_ID: "ap-southeast-1:fc18311d-17c2-49a9-8f5a-10751f500d60"
     }
   };
 
   const prod =  {
     s3: {
       REGION: "ap-southeast-1",
-      BUCKET: "notes-app-uploads-amg"
+      BUCKET: "notes-app-2-api-prod-attachmentsbucket-6aufmpgftypm"
     },
     apiGateway: {
       REGION: "ap-southeast-1",
@@ -26,9 +26,9 @@ const dev =  {
     },
     cognito: {
       REGION: "ap-southeast-1",
-      USER_POOL_ID: "ap-southeast-1_lRPPfJTy5",
-      APP_CLIENT_ID: "cd321b0bpm674jvj0agtc4jif",
-      IDENTITY_POOL_ID: "ap-southeast-1:bf7afc5e-f5e6-4bcd-8676-770d755aef7b"
+      USER_POOL_ID: "ap-southeast-1_wkx4v6vVd",
+      APP_CLIENT_ID: "7jrho5si5rbthr7812vaoo86pf",
+      IDENTITY_POOL_ID: "ap-southeast-1:34f51fa6-95ff-41a7-9657-b02612adb7bd"
     }
   };
 
